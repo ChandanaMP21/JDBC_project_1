04712d19b3063d4c4909528034c279754c6138e2
ChandanaMP21 <151936716+ChandanaMP21@users.noreply.github.com>
2025-05-06 09:31:59 +0530

    Update Program1.java

--- src/com/test/Program1.java
+++ src/com/test/Program1.java
@@ -1,9 +1,13 @@
 package com.test;
-
+import java.util.Scanner;
 public class Program1 {
 
 	public static void main(String[] args) {
+Scanner sc= new Scanner(System.in);
 		System.out.println("hello world");
+		System.out.println("enter the number:");
+		int n=sc.nextInt();
+		System.out.println("square of"+n+"is"+(n*n));
 	}
 
 }
